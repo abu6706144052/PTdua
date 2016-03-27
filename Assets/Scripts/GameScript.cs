5db12215aa724c41d0139543eac56f6f75a5569b
abu6706144052 <aldibakar67@gmail.com>
2016-03-27 21:17:23 +0700

    PT2

---
+++ Assets/Scripts/GameScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameScript : MonoBehaviour {
+	
+	public GameObject panelm, panelb;
+	public enum Seed { EMPTY, PANELM, PANELB } ;
+	public Seed turn;
+	
+	public void SpawnNew(GameObject obj)
+	{
+		if (turn == Seed.PANELM) {
+			Instantiate (panelm, obj.transform.position, Quaternion.identity);
+			turn = Seed.PANELB;
+		} else 
+		{
+			Instantiate(panelb, obj.transform.position, Quaternion.identity);
+			turn = Seed.PANELM;
+		}
+		Destroy (obj.gameObject);
+	}	
+}
---
+++ Assets/Scripts/onhit.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class onhit : MonoBehaviour {
+	
+	public GameObject Camera;
+	public GameScript Script;
+	
+	void Awake()
+	{
+		Script = Camera.GetComponent<GameScript>();
+	}
+	
+	void onMouseDown()
+	{
+		Script.SpawnNew(this.gameObject);
+	}
+}
